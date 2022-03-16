5a8851ca7d8352c2e4559677d06ecc0e47f08062
JudahSan <baraka.judah@yahoo.com>
2022-03-16 19:18:55 +0300

    frontend setup

--- react-django-music-controller/music_controller/api/urls.py
+++ react-django-music-controller/music_controller/api/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import main #RoomView, CreateRoomView
+from .views import RoomView #CreateRoomView
 
 urlpatterns = [
-    path('home', main),
-    path('', main)
+    path('home', RoomView.as_view()),
 ]
--- react-django-music-controller/music_controller/api/views.py
+++ react-django-music-controller/music_controller/api/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+# from django.http import HttpResponse
+from rest_framework import generics
+from .serializers import RoomSerializer #CreateRoomSerializer
+from .models import Room
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
-
+"""
 # Return response function
-
 def main(request):
 	return HttpResponse("<h1>Bonjour</h1>")
+"""
+
+# Create an API view that lets us view a list of all
+#  the different rooms
+
+class RoomView(generics.ListAPIView):
+	queryset = Room.objects.all()
+	serializer_class = RoomSerializer
+
