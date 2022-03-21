a670b87176f958076df2efcc9c8dd3be2b93f44f
JudahSan <baraka.judah@yahoo.com>
2022-03-21 04:05:05 +0300

    component

--- react-django-music-controller/music_controller/frontend/src/components/App.js
+++ react-django-music-controller/music_controller/frontend/src/components/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import HomePage from "./HomePage";
+import CreateRoomPage from "./CreateRoomPage";
+import RoomJoinPage from "./RoomJoinPage";
 
 
 export default class App extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+
+        }
           }
     render() {
-        return <h1>Testing React Code</h1>;
+        return (<div>
+            <HomePage/>
+
+        </div>);
     }
 }
 const appDiv = document.getElementById("app");
+// Create components and pass prop(argument) modify behavior
 render(<App />, appDiv);
                
---
+++ react-django-music-controller/music_controller/frontend/src/components/HomePage.js
@@ -0,0 +1,11 @@
+import React, { Component} from "react";
+import { render } from "react-dom";
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return <p>This is the home page</p>
+    }
+}
