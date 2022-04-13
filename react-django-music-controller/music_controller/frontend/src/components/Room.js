f0a124ec06542967c7cf7f5b25b1bdf69cf2b0c9
JudahSan <baraka.judah@yahoo.com>
2022-04-13 16:59:04 +0300

    Room

--- react-django-music-controller/music_controller/frontend/src/components/Room.js
+++ react-django-music-controller/music_controller/frontend/src/components/Room.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { Grid, Button, Typography } from '@material-ui/core';
+import { Link } from "react-router-dom"
+
 
 export default class Room extends Component {
   constructor(props) {
@@ -10,6 +13,7 @@ export default class Room extends Component {
     };
     this.roomCode = this.props.match.params.roomCode;
     this.getRoomDetails();
+    this.leaveButtonPressed = this.leaveButtonPressed.bind(this);
   }
 
   getRoomDetails() {
@@ -24,14 +28,50 @@ export default class Room extends Component {
       });
   }
 
+  leaveButtonPressed() {
+
+  }
+
   render() {
     return (
-      <div>
+
+      <Grid container spacing={1}>
+        <Grid item xs={12} align="center">
+          <Typography variant="h4" component="h4">
+            Code: {this.roomCode}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+        <Typography variant="h9" component="h9">
+          Votes: {this.state.votesToSkip}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+        <Typography variant="h9" component="h9">
+          Guest Can Pause: {this.state.guestCanPause.toString()}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+        <Typography variant="h9" component="h9">
+          Host: {this.state.isHost.toString()}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button variant="contained" color="secondary" to="/" component={Link}>
+            Leave Room
+          </Button>
+        </Grid>
+      </Grid>
+
+      
+    );
+  }
+}
+/*
+<div>
         <h3>{this.roomCode}</h3>
         <p>Votes: {this.state.votesToSkip}</p>
         <p>Guest Can Pause: {this.state.guestCanPause.toString()}</p>
         <p>Host: {this.state.isHost.toString()}</p>
       </div>
-    );
-  }
-}
+*/
