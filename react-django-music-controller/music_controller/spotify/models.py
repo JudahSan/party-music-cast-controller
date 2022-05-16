ab1c74b0e3c31b95ed5dffe02aaba1b454ecbe94
JudahSan <baraka.judah@yahoo.com>
2022-05-16 03:37:45 +0300

    Finalization

--- react-django-music-controller/music_controller/spotify/models.py
+++ react-django-music-controller/music_controller/spotify/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from api.models import Room
 
 # Create your models here.
 class SpotifyToken(models.Model):
@@ -8,4 +9,14 @@ class SpotifyToken(models.Model):
     access_token = models.CharField(max_length=150)
     expires_in = models.DateTimeField()
     token_type = models.CharField(max_length=50)
-    
+
+# store user votes
+class Vote(models.Model):
+    user = models.CharField(max_length=50, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    song_id = models.CharField(max_length=50)
+    #foreign key
+    # pass an instance of another object
+    # on delete
+    # cascade down an delete anything referencing the room
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
